4fddc21af6837adc1eb7076f35c97676c2e4b9f7
saikiran1224 <knvrssaikiran@gmail.com>
2020-10-29 00:36:42 +0530

    Completed Getting Started Activity

---
+++ app/src/main/java/com/umang/stumate/SignInActivity.kt
@@ -0,0 +1,28 @@
+package com.umang.stumate
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val backButton: ImageView = findViewById(R.id.welcomeBackButton)
+        val signInButton: Button = findViewById(R.id.btnSignIn)
+
+        backButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSignIn.setOnClickListener {
+            Toast.makeText(this, "Successfully Signed In !!!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
