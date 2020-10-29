7e0e6111ab26792d185aa7b14cc9fc9119c23e0e
prathyusha kuppili <prathyusha.kuppili@gmail.com>
2020-10-29 21:36:05 +0530

    Made SignUp Page

---
+++ app/src/main/java/com/umang/stumate/SignupActivity.kt
@@ -0,0 +1,11 @@
+package com.umang.stumate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
