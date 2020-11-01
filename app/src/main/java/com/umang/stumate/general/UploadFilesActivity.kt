205580b904c8c2ebbe6b16c266a950979d902a2a
saikiran1224 <knvrssaikiran@gmail.com>
2020-11-01 11:28:10 +0530

    Successfully Completed Upload Files Activity and passed All Test Cases

--- app/src/main/java/com/umang/stumate/general/UploadFilesActivity.kt
+++ app/src/main/java/com/umang/stumate/general/UploadFilesActivity.kt
@@ -1,43 +1,50 @@
 package com.umang.stumate.general
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Intent
-import android.media.Image
 import android.net.Uri
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
+import android.view.View.*
 import android.widget.*
-import androidx.constraintlayout.solver.GoalRow
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.UploadTask
 import com.umang.stumate.R
+import com.umang.stumate.modals.FileUploadData
 import com.umang.stumate.utils.AppPreferences
-import kotlinx.android.synthetic.main.activity_student_details.*
 import kotlinx.android.synthetic.main.activity_upload_files.*
-import kotlin.math.roundToLong
 
 class UploadFilesActivity : AppCompatActivity() {
 
     private lateinit var closeButton: ImageView
 
     val PDF : Int = 0
+    private var fileDownloadUrl: String? = ""
+    private var fileSelected: String? = "0"
+
+    private  var unitNumber: String? = ""
+    private  var fileType: String? = ""
 
     lateinit var uri : Uri
     lateinit var mStorage : StorageReference
 
-    @SuppressLint("ShowToast")
+    private lateinit var database: DatabaseReference
+
+    @SuppressLint("ShowToast", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload_files)
 
         AppPreferences.init(this)
         setUpSubjectList()
+
+        database = FirebaseDatabase.getInstance().reference
 
         mStorage = FirebaseStorage.getInstance().getReference(AppPreferences.studentID.toString())
 
@@ -48,13 +55,179 @@ class UploadFilesActivity : AppCompatActivity() {
             startActivityForResult(Intent.createChooser(intent, "Select File to Upload"),PDF)
         }
 
+        oneUnit.setOnClickListener {
+
+            oneUnit.setBackgroundResource(R.drawable.blue_rounded_button)
+            oneUnit.setTextColor(resources.getColor(R.color.colorWhite))
+
+            unitNumber = "1"
+
+            secondUnit.setBackgroundResource(R.drawable.rounded_button)
+            thirdUnit.setBackgroundResource(R.drawable.rounded_button)
+            fourthUnit.setBackgroundResource(R.drawable.rounded_button)
+            other.setBackgroundResource(R.drawable.rounded_button)
+
+            secondUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            thirdUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            fourthUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            other.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        }
+        secondUnit.setOnClickListener {
+            secondUnit.setBackgroundResource(R.drawable.blue_rounded_button)
+            secondUnit.setTextColor(resources.getColor(R.color.colorWhite))
+
+            unitNumber = "2"
+
+            oneUnit.setBackgroundResource(R.drawable.rounded_button)
+            thirdUnit.setBackgroundResource(R.drawable.rounded_button)
+            fourthUnit.setBackgroundResource(R.drawable.rounded_button)
+            other.setBackgroundResource(R.drawable.rounded_button)
+
+            oneUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            thirdUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            fourthUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            other.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        }
+
+        thirdUnit.setOnClickListener {
+            thirdUnit.setBackgroundResource(R.drawable.blue_rounded_button)
+            thirdUnit.setTextColor(resources.getColor(R.color.colorWhite))
+
+            unitNumber = "3"
+
+            oneUnit.setBackgroundResource(R.drawable.rounded_button)
+            secondUnit.setBackgroundResource(R.drawable.rounded_button)
+            fourthUnit.setBackgroundResource(R.drawable.rounded_button)
+            other.setBackgroundResource(R.drawable.rounded_button)
+
+            oneUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            secondUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            fourthUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            other.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        }
+
+        fourthUnit.setOnClickListener {
+            fourthUnit.setBackgroundResource(R.drawable.blue_rounded_button)
+            fourthUnit.setTextColor(resources.getColor(R.color.colorWhite))
+
+            unitNumber = "4"
+
+            oneUnit.setBackgroundResource(R.drawable.rounded_button)
+            thirdUnit.setBackgroundResource(R.drawable.rounded_button)
+            secondUnit.setBackgroundResource(R.drawable.rounded_button)
+            other.setBackgroundResource(R.drawable.rounded_button)
+
+            oneUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            thirdUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            secondUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            other.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        }
+
+        other.setOnClickListener {
+            other.setBackgroundResource(R.drawable.blue_rounded_button)
+            other.setTextColor(resources.getColor(R.color.colorWhite))
+
+            unitNumber = "Other Files"
+
+            oneUnit.setBackgroundResource(R.drawable.rounded_button)
+            thirdUnit.setBackgroundResource(R.drawable.rounded_button)
+            fourthUnit.setBackgroundResource(R.drawable.rounded_button)
+            secondUnit.setBackgroundResource(R.drawable.rounded_button)
+
+            oneUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            thirdUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            fourthUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+            secondUnit.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        }
+
+        pdfFormat.setOnClickListener {
+            pdfFormat.setBackgroundResource(R.drawable.blue_rounded_button)
+            pdfFormat.setTextColor(resources.getColor(R.color.colorWhite))
+
+            fileType = "PDF"
+
+            pptFormat.setBackgroundResource(R.drawable.rounded_button)
+            pptFormat.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        }
+
+        pptFormat.setOnClickListener {
+            pptFormat.setBackgroundResource(R.drawable.blue_rounded_button)
+            pptFormat.setTextColor(resources.getColor(R.color.colorWhite))
+
+            fileType = "PPT"
+
+            pdfFormat.setBackgroundResource(R.drawable.rounded_button)
+            pdfFormat.setTextColor(resources.getColor(R.color.colorPrimary))
+
+        }
+
+
         publishFile.setOnClickListener{
-            Toast.makeText(this, " " + AppPreferences.studentID, Toast.LENGTH_SHORT).show()
+
+            val fileName: String = editFileTitle.text.toString()
+            val subjectName: String = subjectNameSpinner.text.toString()
+
+            if(fileSelected.equals("0")) {
+                Toast.makeText(this, "Please select File",Toast.LENGTH_LONG).show()
+            } else if(isNullOrEmpty(fileName)) {
+                edtFileTitle.error = "Please enter Title for File"
+            } else if(isNullOrEmpty(subjectName)) {
+                edtFileTitle.error = null
+                edtSubjectName.error = "Please select related Subject"
+            } else if(unitNumber?.let { it1 -> isNullOrEmpty(it1) }!!) {
+                edtSubjectName.error = null
+                edtFileTitle.error = null
+
+                Toast.makeText(this, "Please choose Unit Number",Toast.LENGTH_LONG).show()
+            } else if(fileType?.let { it1 -> isNullOrEmpty(it1) }!!){
+                edtSubjectName.error = null
+                edtFileTitle.error = null
+                Toast.makeText(this, "Please choose File Type",Toast.LENGTH_LONG).show()
+            } else {
+                edtSubjectName.error = null
+                edtFileTitle.error = null
+
+                edtFileTitle.isEnabled = false
+                editFileTitle.isEnabled = false
+                edtSubjectName.isEnabled = false
+                deleteIcon.isEnabled = false
+
+                oneUnit.isEnabled = false
+                secondUnit.isEnabled = false
+                thirdUnit.isEnabled = false
+                fourthUnit.isEnabled = false
+                other.isEnabled = false
+
+                pptFormat.isEnabled = false
+                pdfFormat.isEnabled = false
+
+                upload(fileName,subjectName,unitNumber,fileType)
+
+            }
+
         }
 
         closeButton = findViewById(R.id.closeButton)
         closeButton.setOnClickListener {
             startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        deleteIcon.setOnClickListener {
+
+            uploadFileButton.text = "Upload File"
+            statusIcon.visibility = VISIBLE
+            deleteIcon.visibility = GONE
+            uploadFileButton.isEnabled = true
+
+            greenStatusIcon.visibility = GONE
+
+
         }
     }
 
@@ -62,22 +235,32 @@ class UploadFilesActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK) {
             uri = data!!.data!!
-            upload()
+
+            fileSelected = "1"
+            statusIcon.visibility = GONE
+            greenStatusIcon.visibility = VISIBLE
+            uploadFileButton.text = "File Selected !"
+            uploadFileButton.isEnabled = false
+
+            deleteIcon.visibility = VISIBLE
+
+            //upload()
         }
     }
 
-    private fun upload() {
+    @SuppressLint("SetTextI18n")
+    private fun upload(fileName: String?, subjectName: String?, unitNumber: String?, fileType: String?) {
 
-        var mReference = mStorage.child(uri.lastPathSegment!!)
+        var mReference = mStorage.child(fileName.toString())
+
+        Toast.makeText(this, "Upload in Progress, Please don't close this Window",Toast.LENGTH_SHORT).show()
 
         mReference.putFile(uri).addOnProgressListener {
                 (bytesTransferred, totalByteCount) ->
 
-            uploadFileButton.text = "Uploading..."
-            uploadFileButton.isEnabled = false
-            statusIcon.visibility = GONE
-
             val progress = (100.0 * bytesTransferred) / totalByteCount
+
+            publishFile.visibility = INVISIBLE
 
             customProgressBar.visibility = VISIBLE
             txtProgress.visibility = VISIBLE
@@ -85,52 +268,54 @@ class UploadFilesActivity : AppCompatActivity() {
             customProgressBar.progress =  progress.toInt()
             txtProgress.text = "${progress.toInt()}%"
 
+
         }.continueWithTask { task ->
             if (!task.isSuccessful) {
                 task.exception?.let {
                     throw it
                 }
             }
+
             mReference.downloadUrl
 
-        } .addOnCompleteListener { task ->
+        }.addOnCompleteListener { task ->
             if (task.isSuccessful) {
 
-                val downloadUri = task.result
+               var downloadUri = task.result.toString()
 
                 customProgressBar.visibility = GONE
                 txtProgress.visibility = GONE
 
-                uploadFileButton.text = "Uploaded !"
-                uploadFileButton.isEnabled = false
+                publishFile.visibility = VISIBLE
+                publishFile.text = "Successfully Uploaded !"
+                publishFile.setBackgroundResource(R.drawable.green_rounded_button)
 
-                greenStatusIcon.visibility = VISIBLE
+                val fileData = FileUploadData(fileName,subjectName,unitNumber,fileType, downloadUri.toString())
+                database.child(AppPreferences.studentID).child("files_data").push().setValue(fileData)
+
+                var dialog = Dialog(this)
+                dialog.setContentView(R.layout.upload_success_layout)
+                dialog.setCanceledOnTouchOutside(false)
+                dialog.setCancelable(false)
+                dialog.findViewById<MaterialButton>(R.id.back_to_home).setOnClickListener {
+                    dialog.dismiss()
+                    startActivity(Intent(this,HomeActivity::class.java))
+                    finish()
+                }
+                dialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+                dialog.show()
 
             } else {
                 // Handle failures
                 // ...
-                Toast.makeText(this,"Error !", Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"Something Error Occurred !", Toast.LENGTH_LONG).show()
+                startActivity(Intent(this,HomeActivity::class.java))
+                finish()
 
             }
         }
-
-
-
-/*
-        try {
-            mReference.putFile(uri).addOnSuccessListener { taskSnapshot: UploadTask.TaskSnapshot? ->
-                if(taskSnapshot.)
-
-                Toast.makeText(this, " "+url.toString(), Toast.LENGTH_LONG).show()
-            }
-        }catch (e: Exception) {
-            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
-        }
-*/
 
     }
-
-
 
 
     private fun setUpSubjectList() {
@@ -139,6 +324,13 @@ class UploadFilesActivity : AppCompatActivity() {
             R.layout.list_item, subjectNames)
         (subjectNameSpinner as? AutoCompleteTextView)?.setAdapter(adapter)
     }
+
+    private fun isNullOrEmpty(str: String): Boolean {
+        if (str != null && !str.trim().isEmpty())
+            return false
+        return true
+    }
+
 
 }
 
---
+++ app/src/main/java/com/umang/stumate/modals/FileUploadData.kt
@@ -0,0 +1,12 @@
+package com.umang.stumate.modals
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class FileUploadData (
+    var fileName: String?,
+    var subjectName: String?,
+    var unitNumber: String?,
+    var fileType: String?,
+    var fileURL: String?
+)
